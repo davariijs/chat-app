f535640f5245b8373f5fd1b52661fceb2de763f5
davariijs <atuor127@gmail.com>
2025-06-16 02:51:06 +0330

    refactor: remove connection and disconnection logs from socket server

--- socket-server.ts
+++ socket-server.ts
@@ -8,15 +8,12 @@ const io = new Server(3001, {
 });
 
 io.on("connection", (socket: Socket) => {
-  console.log("A user connected:", socket.id);
 
   socket.on("send_message", (data: { text: string }) => {
     socket.broadcast.emit("receive_message", data);
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
+  socket.on("disconnect", () => {});
 });
 
 console.log("Socket.IO TypeScript server running on port 3001");
